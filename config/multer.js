9915cd7c79bb7ac852351f6ed8932dbfe794d533
Tushar57517 <sainitushar575@gmail.com>
2025-07-31 20:11:34 +0530

    enabled cloudinary for media storage

--- config/multer.js
+++ config/multer.js
@@ -1,14 +1,25 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.js";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname.replace(/\s/g, ""));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname.replace(/\s/g, ""));
+//   },
+// });
 
-const upload = multer({ storage: storage });
+const storage = new CloudinaryStorage({
+  cloudinary, 
+  params: {
+    folder: "posts",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    resource_type: "auto"
+  }
+})
+
+const upload = multer({ storage });
 
 export default upload;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import boardRoutes from "./routes/board.routes.js";
 import likeRoutes from "./routes/like.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import followRoutes from "./routes/follow.routes.js";
-import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -18,7 +18,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use("/uploads", express.static("uploads"));
 
 app.use("/auth", userRoutes);
 app.use("/posts", postRoutes);
